c833b13fe9f48a795b772a89dec2da2abda1b761
Aditya Raj <aditya@primalistas.com>
2012-09-12 13:03:21 +0530

    Exercise 1 - Sandwiches

---
+++ mysql/exercise1/sandwiches/schema.sql
@@ -0,0 +1,99 @@
+-- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (i486)
+--
+-- Host: localhost    Database: vt_sandwich
+-- ------------------------------------------------------
+-- Server version	5.1.41-3ubuntu12.10
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locations` (
+  `lname` varchar(255) DEFAULT NULL,
+  `phone` varchar(8) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES ('Lincoln','683 4523','Lincoln Place'),('O\'Neill\'s','674 2134','Pearse St'),('Old Nag','767 8132','Dame St'),('Buttery','702 3421','College St');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sandwiches`
+--
+
+DROP TABLE IF EXISTS `sandwiches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sandwiches` (
+  `location` varchar(255) DEFAULT NULL,
+  `bread` varchar(255) DEFAULT NULL,
+  `filling` varchar(255) DEFAULT NULL,
+  `price` float DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sandwiches`
+--
+
+LOCK TABLES `sandwiches` WRITE;
+/*!40000 ALTER TABLE `sandwiches` DISABLE KEYS */;
+INSERT INTO `sandwiches` VALUES ('Lincoln','Rye','Ham',1.25),('O\'Neill\'s','White','Cheese',1.2),('O\'Neill\'s','Whole','Ham',1.25),('Old Nag','Rye','Beef',1.35),('Buttery','White','Cheese',1),('O\'Neill\'s','White','Turkey',1.35),('Buttery','White','Ham',1.1),('Lincoln','Rye','Beef',1.35),('Lincoln','White','Ham',1.3),('Old Nag','Rye','Ham',1.4);
+/*!40000 ALTER TABLE `sandwiches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tastes`
+--
+
+DROP TABLE IF EXISTS `tastes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tastes` (
+  `name` varchar(255) DEFAULT NULL,
+  `filling` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tastes`
+--
+
+LOCK TABLES `tastes` WRITE;
+/*!40000 ALTER TABLE `tastes` DISABLE KEYS */;
+INSERT INTO `tastes` VALUES ('Brown','Turkey'),('Brown','Beef'),('Brown','Ham'),('Jones','Cheese'),('Green','Beef'),('Green','Turkey'),('Green','Cheese');
+/*!40000 ALTER TABLE `tastes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-09-12 12:58:26
